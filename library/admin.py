c2362a715f71354cbc19b77d1cd1a21d063a199e
Andrew Lihonosov <alihonosov@gmail.com>
2020-04-02 22:14:27 +0300

    Admin actions for review

--- library/admin.py
+++ library/admin.py
@@ -10,8 +10,29 @@ class AuthorAdmin(admin.ModelAdmin):
     pass
 
 
+def book_accept(modeladmin, request, queryset):
+    queryset.update(
+        review_status=Book.ReviewStatusChoice.REVIEW_ACCEPTED, reviewer=request.user.id
+    )
+
+
+book_accept.short_description = "Accept selected books"
+
+
+def book_reject(modeladmin, request, queryset):
+    queryset.update(
+        review_status=Book.ReviewStatusChoice.REVIEW_REJECTED, reviewer=request.user.id
+    )
+
+
+book_reject.short_description = "Reject selected books"
+
+
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("author", "title", "uploaded_by", "review_status")
+    list_filter = ("review_status", "reviewer")
+    actions = (book_accept, book_reject)
+
 
 class BookLinkAdmin(admin.ModelAdmin):
     pass
