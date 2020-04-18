93778bfc542533011f662f459e3f429ee65eb09d
Andrew Lihonosov <alihonosov@gmail.com>
2020-04-18 21:41:33 +0300

    Use the new settings names (for boto3)

--- awslibrary/settings.py
+++ awslibrary/settings.py
@@ -129,8 +129,8 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_QUERYSTRING_AUTH = True
 AWS_DEFAULT_ACL = 'private'
-AWS_S3_HOST = 'eu-central-1'
-S3_USE_SIGV4 = True
+AWS_S3_REGION_NAME = 'eu-central-1'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
